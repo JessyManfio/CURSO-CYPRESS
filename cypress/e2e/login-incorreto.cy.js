53f681dec89cf3983c23b63d90539068af928a2e
JessyManfio <jessicamanfiosilva@yahoo.com.br>
2023-12-13 15:17:35 -0300

    Testando login incorreto

---
+++ cypress/e2e/login-incorreto.cy.js
@@ -0,0 +1,8 @@
+describe('Pagina de Login', () => {
+    it('Verificar mensagens de campos obrigatórios', () => {
+      cy.visit('http://localhost:4200/#/home')
+      cy.contains('User name is required!').should('be.visible');
+      cy.contains('Password is required!').should('be.visible');
+      
+    })
+  })
